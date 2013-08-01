b4d17267712b46ba8e81b88dc09e9f2d08b1e65e
Steve Anderson <steve@fuzzwork.co.uk>
2013-08-01 17:48:51 +0200

    clean up

--- monitor.pl
+++ monitor.pl
@@ -1,18 +1,13 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-use Data::Dumper;
 use JSON;
 use Compress::Zlib;
-
-
 use List::Util qw[min max];
-$|=1;
-
-
 use ZMQ::LibZMQ3;
 use ZMQ::Constants qw/ZMQ_SUB ZMQ_SUBSCRIBE/;
 
+$|=1;
 
 
 my $context = zmq_init(1);
@@ -20,14 +15,10 @@ my $sock = zmq_socket($context, ZMQ_SUB);
 zmq_connect($sock, "tcp://localhost:8050");
 zmq_setsockopt($sock,ZMQ_SUBSCRIBE, "");
 
-
-
 while (1) {
     my $msg=zmq_recvmsg($sock);
     last unless defined $msg;
-
     my $json = uncompress(zmq_msg_data($msg));
-
     my $data = decode_json($json);
     if ($data->{resultType} eq "orders")
     {
